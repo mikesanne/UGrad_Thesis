83559127fe145e0acd37951e731390f5cb65d82e
Michael Sanne <29794424+mikesanne@users.noreply.github.com>
2017-10-23 22:57:35 +0200

    Small Changes with Scale - mean instead of median - still not working past image 38

--- main.m
+++ main.m
@@ -10,6 +10,9 @@ close all;
 % Load
 images = imageDatastore('Data/1LoopDown/imagesUndistort_1loopDown');
 K = load('Data/1LoopDown/camIntrinsicDown.txt')';
+
+%images = imageDatastore('Data/rgbd_dataset_freiburg1_xyz/rgb');
+%K = load('Data/rgbd_dataset_freiburg1_xyz/camIntrinsics.txt')';
 cameraParams = cameraParameters('IntrinsicMatrix', K);
 K_h = eye(4,3)*K;
 ViconMatlabImport;
@@ -69,22 +72,21 @@ mpointsMin2_12 = mpoints2_12(minf_12);
 
 
     %% Loop
-for i = 3:50%numel(images.Files)
+for i = 3:38%numel(images.Files)
 close all;
-vId;
     %% Import next image and Determine Relative Pose Between 2 and 3
     I3 = rgb2gray(readimage(images,i));
 
     % Detect Features Image 2
     points3 = detectSURFFeatures(I3, 'MetricThreshold', mtsparm);
     feat3 = extractFeatures(I3, points3, 'Upright', true);
-    %subplot(1,2,1);  ih = imagesc(I2);  colormap(gray);  hold on;  ph = plot(points2);  hold off;
-    %subplot(1,2,2);  ih = imagesc(I3);  colormap(gray);  hold on;  ph = plot(points3);  hold off;
+    subplot(1,2,1);  ih = imagesc(I2);  colormap(gray);  hold on;  ph = plot(points2);  title('Image 1 Points 1');  hold off;
+    subplot(1,2,2);  ih = imagesc(I3);  colormap(gray);  hold on;  ph = plot(points3);  title('Image 2 Points 2');  hold off;
     
     % Putative matches
     mpairs_23 = matchFeatures(feat2, feat3, 'Unique', true);
     mpoints2_23 = points2(mpairs_23(:,1));  mpoints3_23 = points3(mpairs_23(:,2));
-    %fh = figure; showMatchedFeatures(I2, I3, mpoints2_23, mpoints3_23, 'falsecolor'); 
+    fh = figure; showMatchedFeatures(I2, I3, mpoints2_23, mpoints3_23, 'falsecolor'); title('Matched Points'); 
 
     % Pose estimation
     for i=1:1000
@@ -108,10 +110,7 @@ vId;
     mpointsMin2_23 = mpoints2_23(minf_23);
     mpointsMin3_23 = mpoints3_23(minf_23);
     mpairs_23 = mpairs_23(minf_23,:);
-    fh = figure; showMatchedFeatures(I2, I3, mpoints2_23, mpoints3_23, 'falsecolor'); grid on;
-    fh = figure; showMatchedFeatures(I2, I3, mpointsMin2_23, mpointsMin2_23, 'falsecolor'); grid on; 
-%     subplot(1,2,1);  ih = imagesc(I2);  colormap(gray);  hold on;  ph = plot(points2);  hold off;
-%     subplot(1,2,2);  ih = imagesc(I3);  colormap(gray);  hold on;  ph = plot(points3);  hold off;
+    fh = figure; showMatchedFeatures(I2, I3, mpointsMin2_23, mpointsMin2_23, 'falsecolor'); grid on; title('Matched Points Inliers'); 
     
     % Camera matrices
     R2_23 = eye(3);  t2_23 = zeros(1,3);
@@ -120,28 +119,24 @@ vId;
     P3_23 = cameraMatrix(cameraParams, R3_23, t3_23);
 
      %% Implement RANSAC to remove 3D outliers
-    wpa_23 = triangulate(mpointsMin2_23, mpointsMin3_23, P2_23, P3_23);
-    fh = figure;  sh = scatter3(wpa_23(:,1)',wpa_23(:,2)',wpa_23(:,3)','r.');  axis equal; title('Refined 3D Points');
-    
-    % Max Distance a point is allowed to be away from the plane
-    maxDistance = 1;
-    
-    % Reference Vector for Plane
-%   referenceVector = [0, 0, 1];
-    
-    % Computes the model equation for the line and outputs an array of
-    % inlier and outlierIndices
-    ptCloud = pointCloud(wpa_23);
-%    [model, inlierIndices, outlierIndices] = pcfitplane(ptCloud, maxDistance, referenceVector);
-    [model, inlierIndices, outlierIndices] = pcfitplane(ptCloud, maxDistance);
-    wp_23 = wpa_23(inlierIndices,:);
-    %fh = figure;  sh = scatter3(wp_23(:,1)',wp_23(:,2)',wp_23(:,3)','r.');  axis equal;
-    
-    mpointsMin2_23 = mpointsMin2_23(inlierIndices,:);
-    mpointsMin3_23 = mpointsMin3_23(inlierIndices,:);
-    
-    wpa_23 = triangulate(mpointsMin2_23, mpointsMin3_23, P2_23, P3_23);
-    fh = figure;  sh = scatter3(wpa_23(:,1)',wpa_23(:,2)',wpa_23(:,3)','r.');  axis equal; title('Refined 3D Points');
+%     wpa_23 = triangulate(mpointsMin2_23, mpointsMin3_23, P2_23, P3_23);
+%     fh = figure;  sh = scatter3(wpa_23(:,1)',wpa_23(:,2)',wpa_23(:,3)','r.');  axis equal; title('3D Points');
+%     
+%     % Max Distance a point is allowed to be away from the plane
+%     maxDistance = 0.5;
+%     
+%     referenceVector = [0, 0, 1];
+%     % Computes the model equation for the line and outputs an array of
+%     % inlier and outlierIndices
+%     ptCloud = pointCloud(wpa_23);
+%     [model, inlierIndices, outlierIndices] = pcfitplane(ptCloud, maxDistance, referenceVector);
+%     wp_23 = wpa_23(inlierIndices,:);
+%     
+%     mpointsMin2_23 = mpointsMin2_23(inlierIndices,:);
+%     mpointsMin3_23 = mpointsMin3_23(inlierIndices,:);
+%     
+%     wpa_23 = triangulate(mpointsMin2_23, mpointsMin3_23, P2_23, P3_23);
+%     fh = figure;  sh = scatter3(wpa_23(:,1)',wpa_23(:,2)',wpa_23(:,3)','r.');  axis equal; title('Refined 3D Points');
 
     %% Match points in 2D between respective image planes using common features
    
@@ -155,7 +150,10 @@ vId;
     %% Get X' in homogeneous form from inlier points
     wpa_23 = triangulate(points2b_123, points3_123, P2_23, P3_23);
     wph_23 = [wpa_23 zeros(size(wpa_23,1),1)+1]';
-    fh = figure;  sh = scatter3(wpa_23(:,1)',wpa_23(:,2)',wpa_23(:,3)','r.');  axis equal; title('Matched 3D points');
+    fh = figure;  sh = scatter3(wpa_23(:,1)',wpa_23(:,2)',wpa_23(:,3)','r.');  axis equal; title('Matched 3D points Image 2&3');
+
+    wpa_23 = triangulate(points1_123, points2_123, P1_12, P2_12);
+    fh = figure;  sh = scatter3(wpa_23(:,1)',wpa_23(:,2)',wpa_23(:,3)','r.');  axis equal; title('Matched 3D points Image 1&2');
 
      %% Implement RANSAC to remove 3D outliers
 %     % Max Distance a point is allowed to be away from the plane
@@ -196,7 +194,7 @@ vId;
          wp2_23 = triangulate(points2b_123(j+1), points3_123(j+1), P2_23, P3_23);
          distance2_1 = sqrt ((wp2_23(1)-wp1_23(1))^2+(wp2_23(2)-wp1_23(2))^2+(wp2_23(3)-wp1_23(3))^2);
          
-        % Alternate Distance Formulation (Distance from camera 2 to point)
+        % Alternative Distance Formulation (Distance from camera 2 to point)
 %        wp1_12 = triangulate(points1_123(j), points2_123(j), P1_12, P2_12);
 %        c2_Location = v.Views.Location{vId-1};
 %        distance1_1 = sqrt ((wp1_12(1)-c2_Location(1))^2+(wp1_12(2)-c2_Location(2))^2+(wp1_12(2)-c2_Location(3))^2);
@@ -211,16 +209,13 @@ vId;
     end
     
     % Compute scale difference between 2 points
-    scale = median(scaleVector);
+    %scale = median(scaleVector);
+    scale = trimmean(scaleVector, 25);
     if (scale > 5)
         disp('Hello');   
     end
     scaleMatrix = eye(4);
     scaleMatrix(1:3,1:3) = scale*scaleMatrix(1:3,1:3);
-    
-    %% Get X" - Correctly scaled X'
-    wph_23 = scaleMatrix*wph_23;
-    %fh = figure;  sh = scatter3(scale*wpa_23(:,1)',scale*wpa_23(:,2)',scale*wpa_23(:,3)','r.');  axis equal;
 
     %% Perform Rotation from X" to X
     R2_23 = R2_12;
@@ -270,6 +265,7 @@ vId;
     %% Compute world points
     wpi_12 = triangulate(points1_123, points2_123, P1_12, P2_12);
     wpi_23 = triangulate(points2b_123, points3_123, P2_23, P3_23);
+    fh = figure;  sh = scatter3(wpi_23(:,1)',wpi_23(:,2)',wpi_23(:,3)','r.');  axis equal; title('3D Points Image 2&3 Scaled');
     
     %% Plot
     figure;  %axis([-10, 10, -10, 10, 0, 20]);  
@@ -288,7 +284,7 @@ vId;
     %sh = scatter3(wph_23(:,1),wph_23(:,2),wph_23(:,3),'b.');
     title('Previous 3 Cameras and matched points');
 
-    figure;  axis([-10, 10, -10, 10, 0, 20]);  
+    figure;  %axis([-10, 10, -10, 10, 0, 20]);  
     view(gca, 3);
     set(gca, 'CameraUpVector', [0, -1, 0]);  camorbit(gca, -120, 0, 'data', [0, 1, 0]);
     grid on;  xlabel('X');  ylabel('Y');  zlabel('Z');  hold on;
